73b005b7487e7a0ab261970b78555317bec580c1
Levy Morales <levy7165morales@gmail.com>
2024-05-17 00:07:54 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -16,3 +16,28 @@ for (let i = 0; i < skills.length; i++) {
     skill.textContent = skills[i];
     skillsList.appendChild(skill);
 }
+//Messages
+let messageForm = document.forms.leave_message;
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let usersName = messageForm.usersName.value;
+    let usersEmail = messageForm.usersEmail.value;
+    let usersMessage = messageForm.usersMessage.value;
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+    let messageSection = document.getElementById('messages');
+    let messageList = messageSection.querySelector('ul');
+    let newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}"> ${usersName}</a><span>: ${usersMessage}</span>`;
+    let removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.setAttribute('type', 'button');
+    removeButton.addEventListener('click', function(event){
+        let entry = removeButton.parentNode;
+        entry.removeChild;
+    });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+});
